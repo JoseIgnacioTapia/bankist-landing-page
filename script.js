d67a3cccf357e7a7ff13ec2b9cf21f3170f0cb28
JoseIgnacioTapia <josignaciot@gmail.com>
2021-01-17 20:58:36 -0300

    Implementing a Better Way Sticky Navigation with The Intersection Observer API

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
-const header = document.querySelector('header');
+// const header = document.querySelector('header');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
 const tabsContainer = document.querySelector('.operations__tab-container');
@@ -38,18 +38,18 @@ document.addEventListener('keydown', function (e) {
 });
 
 // Creating and inserting a message element ////////////////////
-const message = document.createElement('div');
-message.classList.add('cookie-message');
-message.innerHTML = 'We use cookied for improved functionality and analitycs. <button class="btn btn--close--cookie">Got it!</button>';
+// const message = document.createElement('div');
+// message.classList.add('cookie-message');
+// message.innerHTML = 'We use cookied for improved functionality and analitycs. <button class="btn btn--close--cookie">Got it!</button>';
 
-header.prepend(message);
+// header.prepend(message);
 
 // Delete cookie message element
-document
-  .querySelector('.btn--close--cookie')
-  .addEventListener(
-    'click', () => message.remove()
-  );
+// document
+//   .querySelector('.btn--close--cookie')
+//   .addEventListener(
+//     'click', () => message.remove()
+//   );
 
 // Smooth Scroll ///////////////////////////////////////////
 
@@ -128,13 +128,49 @@ nav.addEventListener('mouseover', handHover.bind(0.5));
 nav.addEventListener('mouseout', handHover.bind(1));
  
 // Sticky Navigation //////////////////////////////
-const initialCoords = section1.getBoundingClientRect();
-console.log(initialCoords);
+// const initialCoords = section1.getBoundingClientRect();
+// console.log(initialCoords);
 
-window.addEventListener('scroll', function() {
-  console.log(window.scrollY);
+// window.addEventListener('scroll', function() {
+//   console.log(window.scrollY);
 
-  if (window.scrollY > initialCoords.top) nav.classList.add('sticky');
-  else nav.classList.remove('sticky');
+//   if (window.scrollY > initialCoords.top) nav.classList.add('sticky');
+//   else nav.classList.remove('sticky');
   
-});
+// });
+
+// Sticky navigation: Intersection Observer API
+// const obsCallback = function (entries, observer) {
+//   entries.forEach(entry => {
+//     console.log(entry);
+//   });
+// }
+
+// const obsOptions = {
+//   root: null,
+//   threshold: [0, 0.2],
+// }
+
+// const observer = new IntersectionObserver(obsCallback, obsOptions);
+// observer.observe(section1);
+
+const header = document.querySelector('.header');
+const navHeight = nav.getBoundingClientRect().height;
+console.log(navHeight);
+
+const stickyNav = function(entries) {
+  const [entry] = entries;
+  console.log(entry);
+
+  if(!entry.isIntersecting) nav.classList.add('sticky')
+  else nav.classList.remove('sticky');
+};
+
+const headerObserver = new IntersectionObserver
+  (stickyNav, {
+    root: null,
+    threshold: 0,
+    rootMargin: `-${navHeight}px`,
+  });
+
+headerObserver.observe(header);
